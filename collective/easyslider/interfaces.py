f44a41f85c0023f4bb5f54de7297daf7dc5bc2bc
Espen Moe-Nilssen <espen@medialog.no>
2012-06-18 10:58:31 +0200

    Revert "Added skip navigation settin in interfaces.py and li.slide float left"
    This reverts commit f24456f5c5812c3ab65df0c22b0e7a9cce4dafbb.

--- collective/easyslider/interfaces.py
+++ collective/easyslider/interfaces.py
@@ -210,9 +210,7 @@ class ISliderSettings(Interface):
             SimpleTerm(value='skip_even', token='skip_even',
                        title='Skip Even Slides'),
             SimpleTerm(value='skip_odd', token='skip_odd',
-                       title='Skip Odd Slides'),
-            SimpleTerm(value='horizontal_slides', token='horizontal_slides',
-                       title='Use horizontal slides setting')
+                       title='Skip Odd Slides')
         ])
     )
 
