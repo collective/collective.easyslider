bd73b0e664c07978493ce239a797ab80193b8f4f
MrTango <md@derico.de>
2022-10-26 15:33:34 +0300

    cleanup

--- src/collective/easyslider/Extensions/Install.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-
-def uninstall(self, reinstall=False):
-    if not reinstall:
-        ps = getToolByName(self, "portal_setup")
-        ps.runAllImportStepsFromProfile("profile-collective.easyslider:uninstall")
